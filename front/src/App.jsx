b0e6e9578ef051be5a7e17ab4dc6768d3ca4fcb8
Romain <hercez92@hotmail.fr>
2022-02-22 18:22:57 +0100

    rename Home to Main

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,16 @@
-import { Fragment } from "react";
 import Header from "./components/Header";
-import Home from "./pages/Home/index"
-import PostsContainer from "./pages/Home/PostsContainer"
+import Main from "./pages/Home/index"
 import { ApiDatasProvider } from "./utils/context/ApiDatas"
 
 function App() {
 
     return (
-        <Fragment>
+        <>
             <Header />
             <ApiDatasProvider>
-                <Home>
-                    <PostsContainer />
-                </Home>
+                <Main />
             </ApiDatasProvider>
-        </Fragment>
+        </>
     );
 }
 
