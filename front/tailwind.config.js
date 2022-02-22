dd3ca9bbc486bd19d2dd64f44facb69165ff8158
Romain <hercez92@hotmail.fr>
2022-02-22 18:14:41 +0100

    update dependencies
    add tailwindcss, typescript, react-testing-library, react-router, postcss, autoprefixer

---
+++ front/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      "flex-center": {
+        "display": "flex",
+        "justify-content": "center",
+        "align-content": "center"
+      }}, 
+  },
+  plugins: [],
+}
