96b4dc24a2c509c8cae851d0fa22877805a0cbf1
Romain <hercez92@hotmail.fr>
2022-02-22 18:16:04 +0100

    add post route

--- back/controllers/forum.js
+++ back/controllers/forum.js
@@ -1,8 +1,20 @@
 const Posts = require("../domain/data/Posts")
+const InMemoryForumRepository = require("../domain/mock/InMemoryForumRepository")
+
+const inMemoryForumRepository = new InMemoryForumRepository(Posts)
 
 exports.getAllPosts = (req, res, next) => {
     try {
-        res.status(201).json(Posts);
+        res.status(200).json(Posts);
+    } catch (error) {
+        res.status(404).json(error);
+    }
+}
+
+exports.postPost = (req, res, next) => {
+    try {
+        inMemoryForumRepository.savePost(req.body)
+        res.status(201).json({message: "Saved !"});
     } catch (error) {
         res.status(404).json(error);
     }
--- back/routes/forum.js
+++ back/routes/forum.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const forumController = require('../controllers/forum')
 
 router.get('/', forumController.getAllPosts);
+router.post("/", forumController.postPost);
 module.exports = router;
