3752be6f9abaaf63c9cc834cee30bdc3e5555b46
Romain <hercez92@hotmail.fr>
2022-02-23 15:25:47 +0100

    switch from styled-components to tailwind

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,24 +1,10 @@
 import logo from '../assets/icon-left-font-monochrome-white.svg'
-import styled from "styled-components"
-
-const StyledHeader = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 24px;
-    background-color: #061c40;
-    box-shadow: 2px 0 15px 2px #061c40;
-`
-const Logo = styled.img`
-    display: flex;
-    max-height: 50px;
-`
 
 export default function Header() {
 
     return (
-        <StyledHeader>
-            <Logo src={logo} alt="Groupomania"/>
-        </StyledHeader>
+        <header className="flex align-center justify-center p-6 bg-blue-900">
+            <img src={logo} alt="Groupomania" className="flex max-h-10" />
+        </header>
     )
 }
