e3e1b77f6a3cf5ad1e2766894907bf393e199b73
Romain <hercez92@hotmail.fr>
2022-02-23 15:27:58 +0100

    split into components

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,19 +1,13 @@
-import { Route, Routes, Link } from 'react-router-dom'
-import PostsContainer from "./PostsContainer"
-import NewPost from "../NewPost/index"
-import Post from "../Post/index"
+import MainContainer from '../../components/MainContainer'
+import MainContent from '../../components/MainContent'
+import SideContent from '../../components/SideContent'
 
 export default function Main() {
 
     return (
-        <main className="flex justify-center bg-gray-200 py-10 md:px-6">
-            <div className='flex flex-col justify-center w-full'>
-                <Routes>
-                    <Route path="/" element={<PostsContainer />} />
-                    <Route path='/:id' element={<Post />} />
-                    <Route path="newpost" element={<NewPost />} />
-                </Routes>
-            </div>
-        </main>
+        <MainContainer children={undefined}>
+            <MainContent />
+            <SideContent />
+        </MainContainer>
     )
 }
