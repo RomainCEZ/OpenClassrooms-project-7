9e4022b2ed5352e54e1ea558ce376bba51b31c36
Romain <hercez92@hotmail.fr>
2022-03-02 00:33:47 +0100

    fix style

--- front/src/components/SideContent.tsx
+++ front/src/components/SideContent.tsx
@@ -1,7 +1,7 @@
 export default function SideContent() {
 
     return (
-        <section className="bg-white border border-indigo-900 rounded my-10 p-5 hidden md:flex md:w-2/5 lg:max-w-sm lg:mr-auto">
+        <section className="bg-white h-fit border border-indigo-900 rounded my-10 p-5 hidden md:flex md:w-2/5 lg:max-w-sm lg:mr-auto">
             Side section
         </section>
     )
