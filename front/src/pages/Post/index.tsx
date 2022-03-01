2b899282d97d436c8f5d32e6dcd7c536d27eeac4
Romain <hercez92@hotmail.fr>
2022-03-02 00:41:12 +0100

    add api call to get post

--- front/src/pages/Post/index.tsx
+++ front/src/pages/Post/index.tsx
@@ -1,17 +1,28 @@
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import { ApiDataContext } from "../../utils/context/ApiDatas"
+import { useState, useEffect } from "react"
+import Loader from "../../components/Loader"
+import BlueButton from "../../components/BlueButton"
+import {Post} from "../../utils/interfaces/Post"
+import { apiProvider } from "../../domain/ApiProvider"
 
-export default function Post() {
+export default function Thread() {
 
-    const {posts} = useContext(ApiDataContext)
-    const id = document.location.pathname.split("/")[1]
-    console.log(id)
-    const post = posts.filter( post => Number(id) === post.id).pop()
+    const [post, setPost] = useState<Post>({id: 0, title: "", body: ""})
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const id = Number(document.location.pathname.split("/")[1])
+
+    useEffect( () => {
+        setIsLoading(true)
+        apiProvider.getPostById(id).then(
+            postData => {
+                setPost(postData)
+                setIsLoading(false)
+            })
+    }, [])
 
     return ( 
         <section>
-            <Link to="/" className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white">Retour</Link>
+            <BlueButton path="/">Retour</BlueButton>
+            {isLoading && <Loader />}
             <div className="p-2 rounded h-80 bg-white border border-indigo-900">
                 <h2 className="text-xl font-semibold p-2 border-b-2 border-blue-900">{post.title}</h2>
                 <p className="p-3">{post.body}</p>
