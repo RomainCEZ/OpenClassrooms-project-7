d79be0dbae03f7c461a868511c17f83817c2e24a
Romain <hercez92@hotmail.fr>
2022-03-02 23:05:19 +0100

    add images upload and images route

--- back/src/posts/posts.controller.ts
+++ back/src/posts/posts.controller.ts
@@ -1,18 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, Res } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
+
+@Controller("images")
+export class ImagesController {
+  @Get(":imagename")
+  findImage(@Param("imagename") imagename: string, @Res() res: any) {
+    return res.sendFile(join(process.cwd(), "images/" + imagename))
+  }
+}
+
+class PostPipe {
+  transform(value: string) {
+    return JSON.parse(value)
+  }
+}
 
 @Controller('api/posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  @UseInterceptors(FileInterceptor('file'))
+  create(@UploadedFile() file: Express.Multer.File, @Body("data", PostPipe) createPostDto: CreatePostDto) {
+    if (file) {
+      createPostDto = {
+        ...createPostDto,
+        imageUrl: `http://192.168.0.10:8000/images/${file.filename}`
+      }
+    }
     this.postsService.create(createPostDto);
     return "Post créé !"
   }
-
+  
   @Get()
   findAll() {
     return this.postsService.findAll();
--- back/src/posts/posts.module.ts
+++ back/src/posts/posts.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { PostsController } from './posts.controller';
+import { PostsController, ImagesController } from './posts.controller';
 import { InMemoryPostsRepository } from './mock/InMemoryPostsRepository';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+  'image/png': 'png',
+};
 @Module({
-  controllers: [PostsController],
+  imports: [
+    MulterModule.register({
+      dest: 'images',
+      storage: diskStorage({
+        destination: "./images",
+        filename: (req, file, callback) => {
+            const name = file.originalname.split('.')[0].split(' ').join('_');
+            const extension = MIME_TYPES[file.mimetype];
+            callback(null, `${name}${Date.now()}.${extension}`);
+        }
+      })
+    })
+  ],
+  controllers: [PostsController, ImagesController],
   providers: [PostsService, InMemoryPostsRepository]
 })
 export class PostsModule {}
