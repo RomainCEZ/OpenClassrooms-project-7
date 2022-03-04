8f1a3cce9fcb59eeaa0cb65966dc27896a0c60f0
Romain <hercez92@hotmail.fr>
2022-03-04 22:51:10 +0100

    add x padding

--- front/src/components/BlueButton.tsx
+++ front/src/components/BlueButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function BlueButton({children, path}) {
     return (
-        <Link to={path} className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white">
+        <Link to={path} className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white px-6">
             {children}
         </Link>
     )
