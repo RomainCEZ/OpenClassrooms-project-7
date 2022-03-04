64fdb270066cff3e06d6627c41d41f37c4350b48
Romain <hercez92@hotmail.fr>
2022-03-04 22:50:40 +0100

    move multer config to ti's own file

--- back/src/posts/posts.module.ts
+++ back/src/posts/posts.module.ts
@@ -1,30 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { PostsController, ImagesController } from './posts.controller';
+import { PostsController } from './posts.controller';
 import { InMemoryPostsRepository } from './mock/InMemoryPostsRepository';
 import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { multerConfig } from '../common/MulterConfig';
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpeg',
-  'image/png': 'png',
-};
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: 'images',
-      storage: diskStorage({
-        destination: "./images",
-        filename: (req, file, callback) => {
-            const name = file.originalname.split('.')[0].split(' ').join('_');
-            const extension = MIME_TYPES[file.mimetype];
-            callback(null, `${name}${Date.now()}.${extension}`);
-        }
-      })
-    })
-  ],
-  controllers: [PostsController, ImagesController],
+  imports: [MulterModule.register(multerConfig)],
+  controllers: [PostsController],
   providers: [PostsService, InMemoryPostsRepository]
 })
 export class PostsModule {}
