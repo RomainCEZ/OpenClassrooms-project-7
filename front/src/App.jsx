9481a9a3f6648b6f5b28fc8434eacd234c8fc0b3
Romain <hercez92@hotmail.fr>
2022-03-22 17:34:02 +0100

    add session context

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Main from "./pages/Home/index"
 import { ApiDatasProvider } from "./utils/context/ApiDatas"
+import { SessionProvider } from "./pages/Auth/context/SessionContext"
 
 function App() {
 
     return (
         <>
+            <SessionProvider>
             <Header />
             <ApiDatasProvider>
                 <Main />
             </ApiDatasProvider>
+            </SessionProvider>
         </>
     );
 }
