00aaa4bbfc48fe64a3fcbe387dd08c7077a237a1
Romain <hercez92@hotmail.fr>
2022-03-22 17:26:11 +0100

    rename PostPipe to ParseJsonPipe

--- back/src/posts/posts.controller.ts
+++ back/src/posts/posts.controller.ts
@@ -5,53 +5,53 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
-class PostPipe {
-  transform(value: string) {
-    return JSON.parse(value)
-  }
+class ParseJsonPipe {
+    transform(value: string) {
+        return JSON.parse(value)
+    }
 }
 
 @Controller('api/posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+    constructor(private readonly postsService: PostsService) { }
 
-  @Post()
-  @UseInterceptors(FileInterceptor('file'))
-    createPost(@UploadedFile() file: Express.Multer.File, @Body("data", PostPipe) createPostDto: CreatePostDto) {
-    if (file) {
-      createPostDto = {
-        ...createPostDto,
-        imageUrl: `http://192.168.0.10:8000/images/${file.filename}`
-      }
+    @Post()
+    @UseInterceptors(FileInterceptor('file'))
+    createPost(@UploadedFile() file: Express.Multer.File, @Body("data", ParseJsonPipe) createPostDto: CreatePostDto) {
+        if (file) {
+            createPostDto = {
+                ...createPostDto,
+                imageUrl: `http://192.168.0.10:8000/images/${file.filename}`
+            }
+        }
+        this.postsService.create(createPostDto);
     }
-    this.postsService.create(createPostDto);
-  }
-  
-  @Get()
-  getAllPosts() {
-    return this.postsService.findAll();
-  }
 
-  @Get(':postId')
-  getPostById(@Param('postId') postId: string) {
-    return this.postsService.findOne(+postId);
-  }
-
-  @Patch(':postId')
-  @UseInterceptors(FileInterceptor('file'))
-  updatePostById(@UploadedFile() file: Express.Multer.File, @Body("data", PostPipe) updatePostDto: UpdatePostDto,@Param('postId') postId: string) {
-    if (file) {
-      updatePostDto = {
-        ...updatePostDto,
-        imageUrl: `http://192.168.0.10:8000/images/${file.filename}`
-      }
+    @Get()
+    getAllPosts() {
+        return this.postsService.findAll();
     }
-    console.log(postId)
-    this.postsService.update(+postId, updatePostDto);
-  }
 
-  @Delete(':postId')
-  deletePostById(@Param('postId') postId: string) {
-    this.postsService.delete(+postId);
-  }
+    @Get(':postId')
+    getPostById(@Param('postId') postId: string) {
+        return this.postsService.findOne(+postId);
+    }
+
+    @Patch(':postId')
+    @UseInterceptors(FileInterceptor('file'))
+    updatePostById(@UploadedFile() file: Express.Multer.File, @Body("data", ParseJsonPipe) updatePostDto: UpdatePostDto, @Param('postId') postId: string) {
+        if (file) {
+            updatePostDto = {
+                ...updatePostDto,
+                imageUrl: `http://192.168.0.10:8000/images/${file.filename}`
+            }
+        }
+        console.log(postId)
+        this.postsService.update(+postId, updatePostDto);
+    }
+
+    @Delete(':postId')
+    deletePostById(@Param('postId') postId: string) {
+        this.postsService.delete(+postId);
+    }
 }
