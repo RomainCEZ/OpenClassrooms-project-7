abbe801c03f6ed0585bc344e5801210eedde9487
Romain <hercez92@hotmail.fr>
2022-03-22 17:36:25 +0100

    add hover and shadow

--- front/src/components/BlueButton.tsx
+++ front/src/components/BlueButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function BlueButton({children, path}) {
     return (
-        <Link to={path} className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white px-6">
+        <Link to={path} className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white px-6 hover:bg-blue-700 hover:shadow-sm hover:shadow-blue-800 ">
             {children}
         </Link>
     )
