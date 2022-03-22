094f9f542407c0b38ca7b6ec0bdde07d67161095
Romain <hercez92@hotmail.fr>
2022-03-22 17:34:51 +0100

    change background color

--- front/src/components/MainContainer.tsx
+++ front/src/components/MainContainer.tsx
@@ -1,7 +1,7 @@
 export default function MainContainer({children}) {
 
     return (
-        <main className="flex justify-between bg-gray-200 sm:px-10 gap-x-10 min-h-screen">
+        <main className="flex justify-between bg-gray-100 sm:px-10 gap-x-10 min-h-screen">
             {children}
         </main>
     )
