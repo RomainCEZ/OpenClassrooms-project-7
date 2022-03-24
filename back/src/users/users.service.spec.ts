014c8bbeef7714b934c576f8b9dd8aa770ec9a14
Romain <hercez92@hotmail.fr>
2022-03-24 22:43:12 +0100

    remove jsonwebbtoken and move authentication to auth service

--- back/src/users/users.service.spec.ts
+++ back/src/users/users.service.spec.ts
@@ -1,4 +1,3 @@
-import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { InMemoryUsersRepository } from './mock/InMemoryUsersRepository';
 import { UsersService } from './users.service';
@@ -8,7 +7,6 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [JwtModule.register({ secret: process.env.JWT_TOKEN_SECRET, signOptions: { expiresIn: '24h' } })], //mettre une variable d'environnement
       providers: [UsersService, InMemoryUsersRepository],
     }).compile();
 
