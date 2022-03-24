a714f16a0725668c862e45dbfb21aca2e5546d78
Romain <hercez92@hotmail.fr>
2022-03-24 22:46:04 +0100

    add express  and passport

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,14 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
+import * as session from 'express-session'
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config()
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://192.168.0.10:3000',
+    credentials: true
+  });
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+  app.use(
+    session({
+      secret: `${process.env.SUPER_SECRET_SESSION_SECRET}`,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24h
+    }),
+  );
+  app.use(passport.initialize())
+  app.use(passport.session())
+  
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
