4fa706aab87d93c856ed9dd9a7b7600c8613b12b
Romain <hercez92@hotmail.fr>
2022-03-28 00:51:04 +0200

    change ids from numbers to strings

--- back/src/forum/posts/posts.service.ts
+++ back/src/forum/posts/posts.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import * as fs from "fs"
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
 import { InMemoryPostsRepository } from './mock/InMemoryPostsRepository';
-import { Post } from './interfaces/postInterface';
 
 @Injectable()
 export class PostsService {
@@ -17,15 +17,15 @@ export class PostsService {
     return this.postsRepository.getAllPosts();
   }
 
-  findOne(postId: number): Post {
+  findOne(postId: string): Post {
     return this.postsRepository.getById(postId);
   }
 
-  update(postId: number, updatePostDto: UpdatePostDto) {
+  update(postId: string, updatePostDto: UpdatePostDto) {
     this.postsRepository.update(postId, updatePostDto)
   }
 
-  delete(postId: number) {
+  delete(postId: string) {
     const post = this.postsRepository.getById(postId)
     if (post.imageUrl) {
       const fileName = post.imageUrl.split("/images/")[1]
