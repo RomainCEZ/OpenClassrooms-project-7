ba67c7d62c17ac4daaf62b5f0197ea3d3a34adfd
Romain <hercez92@hotmail.fr>
2022-03-28 00:54:54 +0200

    serve client as static file

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ForumModule } from './forum/forum.module';
 import { ImagesModule } from './images/images.module';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ForumModule, ImagesModule, AuthModule]
+  imports: [ForumModule, ImagesModule, AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+      exclude: ['/api*', '/auth*', '/images*'],
+    })]
 })
 export class AppModule {}
