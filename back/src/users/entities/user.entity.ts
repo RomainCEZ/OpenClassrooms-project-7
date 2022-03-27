c8bb4b26dd5f8655d0cb87c3030bc087a8384c50
Romain <hercez92@hotmail.fr>
2022-03-28 00:37:45 +0200

    add "role" property

--- back/src/users/entities/user.entity.ts
+++ back/src/users/entities/user.entity.ts
@@ -1,12 +1,11 @@
 import { nanoid } from 'nanoid'
-// import { Email } from "./Email.entity";
-// import { UserPassword } from './UserPassword.entity';
 
 interface UserProps {
     id?: string
     email: string
     username: string
     password: string
+    role?: string
 }
 
 export class User {
@@ -14,20 +13,23 @@ export class User {
     readonly email: string
     readonly username: string
     readonly password: string
+    readonly role: string
 
-    constructor({ id, email, username, password }: UserProps) {
+    constructor({ id, email, username, password, role }: UserProps) {
         this.id = id
         this.email = email
         this.username = username
         this.password = password
+        this.role = role
     }
 
-    public static create({ id, email, username, password }: UserProps) {
+    public static create({ id, email, username, password, role }: UserProps) {
         return new User({
             id: id || nanoid(),
             email,
             username,
-            password
+            password,
+            role: role || "user"
           })
     }
 }
