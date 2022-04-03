541eec7ea37674a2ef561f4a6f84c6d8e9d7f450
Romain <hercez92@hotmail.fr>
2022-04-04 00:28:50 +0200

    create comment component

--- front/src/pages/Post/CommentLoader.tsx
+++ front/src/pages/Post/CommentLoader.tsx
@@ -1,7 +1,18 @@
 export default function Comment() {
     return (
-        <div className="h-20 bg-white rounded">
-            <div className="h-20 bg-gray-100 rounded-sm animate-pulse"></div>
-        </div>
+        <>
+            <div className="flex flex-col items-center gap-1 p-2">
+                <div className="border-[16px] border-white border-b-blue-800 hover:border-b-blue-500 cursor-pointer transition-colors"></div>
+                <p>+2</p>
+                <div className="border-[16px] border-white border-t-blue-800 hover:border-t-blue-500 cursor-pointer transition-colors"></div>
+            </div>
+            <div className="w-full flex flex-col items-around p-4 pb-0">
+                <p></p>
+
+                <p className="border-t border-gray-400 text-sm mt-auto p-2 first-letter:capitalize">
+                    <div className="h-20 bg-gray-100 rounded-sm animate-pulse"></div>
+                </p>
+            </div>
+        </>
     );
 }
--- front/src/pages/Post/PostView.tsx
+++ front/src/pages/Post/PostView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, Suspense } from "react";
+import { useState, useEffect, useContext } from "react";
 import BlueButton from "../../components/BlueButton";
 import { Post } from "../../utils/interfaces/Post";
 import { apiProvider } from "../../domain/ApiProvider";
@@ -9,7 +9,8 @@ import { EditorState, convertFromRaw, ContentState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import Comment from "./Comment";
-import CommentLoader from "./CommentLoader";
+// import CommentLoader from "./CommentLoader";
+import ReactTimeAgo from "react-time-ago";
 
 export default function PostView() {
     const [post, setPost] = useState<Post>({
@@ -17,7 +18,24 @@ export default function PostView() {
         title: "",
         body: "",
         editorContent: "",
+        imageUrl: "",
     });
+    const [commentsData, setCommentsData] = useState([
+        {
+            content: "J'adore !!!",
+            author: "Author",
+            timestamp: 1648975642144,
+        },
+    ]);
+
+    const comments = commentsData.map((comment) => (
+        <Comment
+            content={comment.content}
+            author={comment.author}
+            timestamp={comment.timestamp}
+        />
+    ));
+
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const { user } = useContext(SessionContext);
     const id = document.location.pathname.split("/post/")[1];
@@ -31,6 +49,9 @@ export default function PostView() {
                     id: postData.id,
                     title: postData.title,
                     body: editorState,
+                    imageUrl: postData.imageUrl,
+                    timestamp: postData.timestamp,
+                    author: postData.author,
                 });
             } else {
                 const contentState = convertFromRaw(postData.body);
@@ -52,40 +73,52 @@ export default function PostView() {
             {isLoading ? (
                 <PostLoader />
             ) : (
-                <div className="flex flex-col mt-2 p-2 sm:px-5 rounded min-h-80 h-fit bg-white border border-indigo-900 divide-blue-900 divide-y-2">
-                    <h2 className="text-xl font-semibold p-2 sm:px-0">
-                        {post.title}
-                    </h2>
-                    <div className="flex w-full p-2">
+                <div className="flex flex-col mt-2 p-2 sm:px-5 rounded min-h-80 h-fit bg-white border border-indigo-900">
+                    <div className="mb-3 pb-2 border-b-2 border-indigo-900">
+                        <h2 className="text-xl font-semibold p-2 sm:px-0 decoration-2 underline underline-offset-2 text-blue-800">
+                            {post.title}
+                        </h2>
+                        <p className="text-sm ml-2 first-letter:capitalize">
+                            <ReactTimeAgo
+                                date={post.timestamp}
+                                locale="fr-FR"
+                                className="font-bold"
+                            />{" "}
+                            par <span className="font-bold">{post.author}</span>
+                        </p>
+                    </div>
+                    <div className="flex flex-col w-full p-2">
                         {post.imageUrl && (
-                            <img src={post.imageUrl} className="pt-3 w-full" />
+                            <img src={post.imageUrl} className="py-3 w-full " />
                         )}
                         <Editor
                             readOnly
                             toolbarHidden
                             textAlignment="left"
                             editorState={post.body}
-                            wrapperClassName="flex flex-col h-full w-full wrapper-class bg-gray-200 "
+                            wrapperClassName="flex flex-col h-full w-full wrapper-class bg-gray-200 border-b border-indigo-900 "
                             editorClassName="editor-class px-3 bg-white"
                         />
                     </div>
+                    {(user.id === post.authorId || user.role === "admin") && (
+                        <div className="flex justify-end items-center m-1 mr-2 gap-2">
+                            <BlueButton path={`/post/${id}/edit`}>
+                                Éditer
+                            </BlueButton>
+                            <Link
+                                to="/"
+                                className="flex justify-center p-2 px-6 rounded bg-red-900 text-white"
+                                onClick={() => apiProvider.deletePost(id)}
+                            >
+                                Supprimer
+                            </Link>
+                        </div>
+                    )}
                 </div>
             )}
-            {(user.id === post.authorId || user.role === "admin") && (
-                <div className="flex justify-end items-center p-2 gap-2">
-                    <BlueButton path={`/post/${id}/edit`}>Éditer</BlueButton>
-                    <Link
-                        to="/"
-                        className="flex justify-center p-2 px-6 mb-3 rounded bg-red-900 text-white"
-                        onClick={() => apiProvider.deletePost(id)}
-                    >
-                        Supprimer
-                    </Link>
-                </div>
-            )}
-            <Suspense fallback={<CommentLoader />}>
-                <Comment title={post.title} author={post.author} />
-            </Suspense>
+            <div className="flex mt-2 bg-white border border-indigo-800 rounded-sm divide-x divide-indigo-800">
+                {comments}
+            </div>
         </section>
     );
 }
