4ed5d234f11b1fa44d16b197d4640d475964e31e
Romain <hercez92@hotmail.fr>
2022-04-03 03:29:34 +0200

    rename

---
+++ front/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import MainContainer from "../../components/MainContainer";
+import MainContent from "../../components/MainContent";
+
+export default function Main() {
+    return (
+        <MainContainer>
+            <MainContent />
+        </MainContainer>
+    );
+}
---
+++ front/src/pages/Post/PostView.tsx
@@ -0,0 +1,91 @@
+import { useState, useEffect, useContext, Suspense } from "react";
+import BlueButton from "../../components/BlueButton";
+import { Post } from "../../utils/interfaces/Post";
+import { apiProvider } from "../../domain/ApiProvider";
+import { Link } from "react-router-dom";
+import { SessionContext } from "../Auth/context/SessionContext";
+import PostLoader from "./PostLoader";
+import { EditorState, convertFromRaw, ContentState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import Comment from "./Comment";
+import CommentLoader from "./CommentLoader";
+
+export default function PostView() {
+    const [post, setPost] = useState<Post>({
+        id: "",
+        title: "",
+        body: "",
+        editorContent: "",
+    });
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { user } = useContext(SessionContext);
+    const id = document.location.pathname.split("/post/")[1];
+
+    useEffect(() => {
+        apiProvider.getPostById(id).then((postData) => {
+            if (typeof postData.body === "string") {
+                const contentState = ContentState.createFromText(postData.body);
+                const editorState = EditorState.createWithContent(contentState);
+                setPost({
+                    id: postData.id,
+                    title: postData.title,
+                    body: editorState,
+                });
+            } else {
+                const contentState = convertFromRaw(postData.body);
+                const editorState = EditorState.createWithContent(contentState);
+                setPost({
+                    id: postData.id,
+                    title: postData.title,
+                    body: editorState,
+                });
+            }
+            setIsLoading(false);
+        });
+        return;
+    }, []);
+
+    return (
+        <section>
+            <BlueButton path="/">Retour</BlueButton>
+            {isLoading ? (
+                <PostLoader />
+            ) : (
+                <div className="flex flex-col mt-2 p-2 sm:px-5 rounded min-h-80 h-fit bg-white border border-indigo-900 divide-blue-900 divide-y-2">
+                    <h2 className="text-xl font-semibold p-2 sm:px-0">
+                        {post.title}
+                    </h2>
+                    <div className="flex w-full p-2">
+                        {post.imageUrl && (
+                            <img src={post.imageUrl} className="pt-3 w-full" />
+                        )}
+                        <Editor
+                            readOnly
+                            toolbarHidden
+                            textAlignment="left"
+                            editorState={post.body}
+                            wrapperClassName="flex flex-col h-full w-full wrapper-class bg-gray-200 "
+                            editorClassName="editor-class px-3 bg-white"
+                        />
+                    </div>
+                </div>
+            )}
+            {(user.id === post.authorId || user.role === "admin") && (
+                <div className="flex justify-end items-center p-2 gap-2">
+                    <BlueButton path={`/post/${id}/edit`}>Éditer</BlueButton>
+                    <Link
+                        to="/"
+                        className="flex justify-center p-2 px-6 mb-3 rounded bg-red-900 text-white"
+                        onClick={() => apiProvider.deletePost(id)}
+                    >
+                        Supprimer
+                    </Link>
+                </div>
+            )}
+            <Suspense fallback={<CommentLoader />}>
+                <Comment title={post.title} author={post.author} />
+            </Suspense>
+        </section>
+    );
+}
