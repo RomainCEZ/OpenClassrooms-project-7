6ca5f28cfa861f7fa317cb4bbd93ce45f02647da
Romain <hercez92@hotmail.fr>
2022-04-03 03:34:23 +0200

    add author and time from creation to post preview

--- front/src/pages/Home/PostPreview.tsx
+++ front/src/pages/Home/PostPreview.tsx
@@ -1,37 +1,63 @@
-import { Link } from "react-router-dom"
-import { EditorState, convertFromRaw, ContentState } from 'draft-js';
+import { Link } from "react-router-dom";
+import { EditorState, convertFromRaw, ContentState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { useEffect, useState } from "react";
+import ReactTimeAgo from "react-time-ago";
 import DraftjsView from "../../components/Draftjs/DraftjsView";
 
-export default function PostPreview({ id, title, body, src }) {
-
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
+export default function PostPreview({
+    id,
+    title,
+    body,
+    src,
+    author,
+    timestamp,
+}) {
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
     );
 
-    useEffect( () => {
+    useEffect(() => {
         if (typeof body === "string") {
-                    const contentState = ContentState.createFromText(body)
-                    setEditorState(EditorState.createWithContent(contentState))
-                } else {
-                    const contentState = convertFromRaw(body)
-                    setEditorState(EditorState.createWithContent(contentState))
-                }
-            
-            return
-    }, [])
+            const contentState = ContentState.createFromText(body);
+            setEditorState(EditorState.createWithContent(contentState));
+        } else {
+            const contentState = convertFromRaw(body);
+            setEditorState(EditorState.createWithContent(contentState));
+        }
 
+        return;
+    }, []);
     return (
-        <Link to={`/${id}`} className="flex flex-col p-5 bg-white hover:bg-blue-200 w-full border border-indigo-900 rounded">
+        <Link
+            to={`/post/${id}`}
+            className="group flex flex-col px-5 py-4 bg-white w-full border border-indigo-900 rounded hover:border-indigo-700 hover:shadow hover:shadow-indigo-500 transition-all"
+        >
             <article>
-                <h2 className="borderb text-lg font-semibold">{title}</h2>
-                {src ? <img src={src} className="pt-3 cover w-full" />:
-                    <DraftjsView
-                        editorState={editorState}
-                    />
-                }
+                <div className="mb-3 pb-2 border-b-2 border-indigo-900 group-hover:border-indigo-600 transition-all">
+                    <h2 className="mb-2 text-lg font-semibold decoration-2 underline underline-offset-2  text-blue-800 group-hover:text-blue-500 transition-all">
+                        {title}
+                    </h2>
+                    <p className="text-sm ml-2 group-hover:text-gray-600 transition-all">
+                        <ReactTimeAgo
+                            date={timestamp}
+                            locale="fr-FR"
+                            className="font-bold group-hover:text-gray-600 transition-all"
+                        />{" "}
+                        par <span className="font-bold">{author}</span>
+                    </p>
+                </div>
+                <div className="pb-2 border-b-[1px] border-indigo-900 group-hover:border-indigo-600 transition-all">
+                    {src ? (
+                        <img src={src} className="pt-3 w-full object-contain" />
+                    ) : (
+                        <DraftjsView editorState={editorState} />
+                    )}
+                </div>
+                <p className="text-sm px-2 pt-4 group-hover:text-gray-600 transition-all">
+                    x commentaires
+                </p>
             </article>
         </Link>
-    )
+    );
 }
--- front/src/pages/Home/PostsContainer.tsx
+++ front/src/pages/Home/PostsContainer.tsx
@@ -1,42 +1,71 @@
-import { useState, useEffect, useContext } from "react"
-import BlueButton from "../../components/BlueButton"
-import PostPreview from "./PostPreview"
-import { Post } from "../../utils/interfaces/Post"
-import { apiProvider } from "../../domain/ApiProvider"
-import { SessionContext } from "../Auth/context/SessionContext"
-import PostPreviewLoader from "./PostPreviewLoader"
-import { Link } from "react-router-dom"
+import { useState, useEffect, useContext } from "react";
+import BlueButton from "../../components/BlueButton";
+import PostPreview from "./PostPreview";
+import { Post } from "../../utils/interfaces/Post";
+import { apiProvider } from "../../domain/ApiProvider";
+import { SessionContext } from "../Auth/context/SessionContext";
+import PostPreviewLoader from "./PostPreviewLoader";
+import { Link } from "react-router-dom";
 
 export default function PostWrap() {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { loggedIn } = useContext(SessionContext);
 
-    const [posts, setPosts] = useState<Post[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const { loggedIn } = useContext(SessionContext)
-    
-    const postsElements: JSX.Element[] = posts.map( (post: Post) => <PostPreview key={post.id} id={post.id} title={post.title} src={post.imageUrl} body={post.body} />)
-    
-    useEffect( () => {
-        apiProvider.getAllPosts().then( postsData => {
-            setPosts(postsData)
-            setIsLoading(false)
-        })
-    }, [])
+    const postsElements: JSX.Element[] = posts.map((post: Post) => (
+        <PostPreview
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            src={post.imageUrl}
+            body={post.body}
+            author={post.author}
+            timestamp={post.timestamp}
+        />
+    ));
+
+    useEffect(() => {
+        apiProvider.getAllPosts().then((postsData) => {
+            setPosts(postsData);
+            setIsLoading(false);
+        });
+    }, []);
 
     return (
         <>
-            {
-                loggedIn ? 
-                <BlueButton path="newPost">Nouveau post</BlueButton> : 
-                <div className="flex justify-center p-2 mb-3 rounded bg-white border border-indigo-900 px-6">
-                    <Link to='/login' className="text-blue-700 hover:text-blue-400 underline font-bold">Connectez-vous</Link>
-                    <span>ou</span>
-                    <Link to='/signup' className="text-blue-700 hover:text-blue-400 underline font-bold">enregistrez-vous</Link> pour publier votre contenu !
-                </div>
-            }
+            {loggedIn ? (
+                <BlueButton path="newPost">Nouveau post</BlueButton>
+            ) : (
+                <p className="flex flex-col sm:flex-row items-center justify-center p-2 mb-3 rounded bg-white border border-indigo-900 px-6">
+                    <span>
+                        <Link
+                            to="/login"
+                            className="text-blue-700 hover:text-blue-400 underline font-bold transition-all"
+                        >
+                            Connectez-vous
+                        </Link>
+                        &nbsp;ou&nbsp;
+                        <Link
+                            to="/signup"
+                            className="text-blue-700 hover:text-blue-400 underline font-bold transition-all"
+                        >
+                            enregistrez-vous
+                        </Link>
+                    </span>
+                    &nbsp;pour publier votre contenu !
+                </p>
+            )}
             <section className="flex flex-col gap-1 w-full">
-                {isLoading ? <><PostPreviewLoader /><PostPreviewLoader /><PostPreviewLoader /><PostPreviewLoader /></> :
-                postsElements}
+                {isLoading ? (
+                    <>
+                        <PostPreviewLoader />
+                        <PostPreviewLoader />
+                        <PostPreviewLoader />
+                    </>
+                ) : (
+                    postsElements
+                )}
             </section>
         </>
-    )
+    );
 }
