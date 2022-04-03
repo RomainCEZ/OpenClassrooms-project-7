76a2f3c5ac7a0cdf3106f5a14467760bffae411a
Romain <hercez92@hotmail.fr>
2022-04-04 00:16:48 +0200

    remove margin

--- front/src/components/BlueButton.tsx
+++ front/src/components/BlueButton.tsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export default function BlueButton({children, path}) {
+export default function BlueButton({ children, path }) {
     return (
-        <Link to={path} className="flex justify-center p-2 mb-3 rounded bg-blue-900 text-white px-6 hover:bg-blue-700 hover:shadow-sm hover:shadow-blue-800 transition-all">
+        <Link
+            to={path}
+            className="flex justify-center p-2 rounded bg-blue-900 text-white px-6 hover:bg-blue-700 hover:shadow-sm hover:shadow-blue-800 transition-all"
+        >
             {children}
         </Link>
-    )
+    );
 }
