2494490f219b6136b1fab5ec15286a2411e004d8
Romain <hercez92@hotmail.fr>
2022-04-04 00:25:03 +0200

    formating

--- front/src/components/MainContainer.tsx
+++ front/src/components/MainContainer.tsx
@@ -1,8 +1,7 @@
-export default function MainContainer({children}) {
-
+export default function MainContainer({ children }) {
     return (
         <main className="flex justify-between bg-gray-100 sm:px-10 gap-x-10 min-h-screen">
             {children}
         </main>
-    )
+    );
 }
