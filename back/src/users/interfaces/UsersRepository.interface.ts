d0a0ab02f24e206bd11db4655b6d6068bc1a0abb
Romain <hercez92@hotmail.fr>
2022-04-04 21:37:41 +0200

    add data param

--- back/src/users/interfaces/UsersRepository.interface.ts
+++ back/src/users/interfaces/UsersRepository.interface.ts
@@ -1,12 +1,12 @@
 import { User } from "../entities/User";
 
 export interface UsersRepository {
-
+    data: User[]
     // getAllUsers(): UserData[]
 
     saveUser(user: User): void
 
-    getByEmail(email: string): User 
+    getByEmail(email: string): User
 
     getById(id: string): User
 
