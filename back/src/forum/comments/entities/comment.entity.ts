31db2a5e98bb773016608e5400d4fea4d0eb07f6
Romain <hercez92@hotmail.fr>
2022-04-04 21:38:54 +0200

    create comment entity

--- back/src/forum/comments/entities/comment.entity.ts
+++ back/src/forum/comments/entities/comment.entity.ts
@@ -1,1 +1,26 @@
-export class Comment {}
+import { nanoid } from "nanoid";
+import CommentProps from "../interfaces/CommentProps";
+
+export class Comment implements CommentProps {
+    readonly id: string
+    readonly content: string
+    readonly author: string
+    readonly authorId: string
+    readonly timestamp: number
+
+    constructor({ id, content, author, authorId, timestamp }: CommentProps) {
+        this.id = id
+        this.content = content
+        this.author = author
+        this.authorId = authorId
+        this.timestamp = timestamp
+    }
+
+    static create(comment: CommentProps) {
+        return new Comment({
+            ...comment,
+            id: comment.id || nanoid(),
+            timestamp: comment.timestamp || Date.now()
+        })
+    }
+}
