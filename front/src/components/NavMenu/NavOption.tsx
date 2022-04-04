b61d42173c19c80f687188692f77048e91cab42b
Romain <hercez92@hotmail.fr>
2022-04-04 21:51:32 +0200

    format code

--- front/src/components/NavMenu/NavOption.tsx
+++ front/src/components/NavMenu/NavOption.tsx
@@ -3,23 +3,25 @@ import { ReactChild } from "react";
 import { Link } from "react-router-dom";
 
 interface NavOptionProps {
-    handleClick?: Function
-    path: string
-    children: ReactChild
+    handleClick?: Function;
+    path: string;
+    children: ReactChild;
 }
 
 export function NavOption({ handleClick, path, children }: NavOptionProps) {
-
     return (
         <Menu.Item>
             {({ active }) => (
-                <Link 
-                    to={path} 
+                <Link
+                    to={path}
                     className={`
-                        flex justify-center items-center p-4
-                        ${active ? "bg-blue-600" : "" }
+                        flex justify-center items-center p-4 font-bold
+                        ${active ? "bg-blue-600" : ""}
                     `}
-                    onClick={() => handleClick()}>{children}</Link>
+                    onClick={() => handleClick()}
+                >
+                    {children}
+                </Link>
             )}
         </Menu.Item>
     );
