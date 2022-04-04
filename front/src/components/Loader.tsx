b2572bd4c0e13437c5ce874923dc40cb2a195e4f
Romain <hercez92@hotmail.fr>
2022-04-04 18:00:13 +0200

    replace spinner with skeletons

--- front/src/components/Loader.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-    return (
-        <div className="flex flex-col items-center justify-center p-4">
-            <div className="spinner-border animate-spin inline-block w-12 h-12 border-8 rounded-full text-blue-900" role="status"></div>
-            <span>Chargement en cours...</span>
-        </div>
-    )
-}
