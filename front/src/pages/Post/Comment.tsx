85881d1458672bcb186096776d9d0a81c89aed03
Romain <hercez92@hotmail.fr>
2022-04-04 21:52:22 +0200

    animate arrows

--- front/src/pages/Post/Comment.tsx
+++ front/src/pages/Post/Comment.tsx
@@ -2,11 +2,17 @@ import ReactTimeAgo from "react-time-ago";
 
 export default function Comment({ content, author, timestamp }) {
     return (
-        <>
+        <div className="flex divide-x bg-white divide-indigo-800 border border-indigo-800 rounded-sm">
             <div className="flex flex-col items-center gap-1 p-2">
-                <div className="border-[16px] border-transparent border-b-blue-800 hover:border-b-blue-500 cursor-pointer transition-colors"></div>
+                <div
+                    className="border-[16px] border-transparent border-b-blue-700
+                    hover:border-b-blue-500 focus:border-b-blue-500 active:border-b-blue-400 cursor-pointer transition-all"
+                ></div>
                 <p>+2</p>
-                <div className="border-[16px] border-transparent border-t-blue-800 hover:border-t-blue-500 cursor-pointer transition-colors"></div>
+                <div
+                    className="border-[16px] border-transparent border-t-blue-700
+                    hover:border-t-blue-500 focus:border-t-blue-500 active:border-t-blue-400 cursor-pointer transition-all"
+                ></div>
             </div>
             <div className="w-full flex flex-col items-around p-4 pb-0">
                 <p>{content}</p>
@@ -19,6 +25,6 @@ export default function Comment({ content, author, timestamp }) {
                     par <span className="font-bold">{author}</span>
                 </p>
             </div>
-        </>
+        </div>
     );
 }
