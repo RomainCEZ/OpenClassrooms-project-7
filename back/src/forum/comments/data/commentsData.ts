6e4b4fc69da9e6cef2047ea05a903487c618cf24
Romain <hercez92@hotmail.fr>
2022-04-04 21:40:20 +0200

    create placeholders comments

---
+++ back/src/forum/comments/data/commentsData.ts
@@ -0,0 +1,21 @@
+import PostComments from "../interfaces/PostComments";
+
+export const commentsData: PostComments[] = [
+    {
+        postId: "0",
+        comments: [{
+            id: "placeholderComment1",
+            content: "J'adore !!!",
+            author: "admin",
+            authorId: "0",
+            timestamp: 1648975642144,
+        },
+        {
+            id: "placeholderComment2",
+            content: "J'adore !!!",
+            author: "admin",
+            authorId: "0",
+            timestamp: 1648975642144,
+        }]
+    }
+]
