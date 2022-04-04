50cf04017794e18579ea491adf63fd9538700a12
Romain <hercez92@hotmail.fr>
2022-04-04 21:39:57 +0200

    create comments in memory repository

---
+++ back/src/forum/comments/repositories/InMemoryCommentsRepository.ts
@@ -0,0 +1,29 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { commentsData } from "../data/commentsData";
+import { Comment } from "../entities/comment.entity";
+import PostComments from "../interfaces/PostComments";
+import CommentsRepository from "../interfaces/CommentsRepository";
+
+@Injectable()
+export default class InMemoryCommentsRepository implements CommentsRepository {
+    data: PostComments[];
+    constructor() {
+        this.data = commentsData
+    }
+    saveComment(postId: string, comment: Comment) {
+        const postComments = this.getPostCommentsByPostId(postId)
+        postComments.comments.unshift(comment)
+    }
+    getPostCommentsByPostId(postId: string): PostComments {
+        const postComments = this.data.find(postComments => postComments.postId === postId)
+        if (!postComments) {
+            throw new NotFoundException()
+        }
+        return postComments
+    }
+    deletePostCommentsByPostId(postId: string) {
+        this.getPostCommentsByPostId(postId)
+        this.data.filter(postComments => postComments.postId !== postId)
+    }
+
+}
