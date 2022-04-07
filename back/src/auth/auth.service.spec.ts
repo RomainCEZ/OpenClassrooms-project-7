f909e18eb5d126ed62cf58aa3064856af6c48771
Romain <hercez92@hotmail.fr>
2022-04-08 00:34:06 +0200

    remove db connection from test module

--- back/src/auth/auth.service.spec.ts
+++ back/src/auth/auth.service.spec.ts
@@ -3,19 +3,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './guard/local.strategy';
 import { UsersModule } from '../users/users.module';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UserModel } from '../Database/sequelizeModels/User.model';
-import sequelizeConfig from '../Database/sequelize.config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, PassportModule, SequelizeModule.forRoot({
-        ...sequelizeConfig,
-        models: [UserModel],
-      })],
+      imports: [UsersModule, PassportModule],
       providers: [AuthService, LocalStrategy]
     }).compile();
 
