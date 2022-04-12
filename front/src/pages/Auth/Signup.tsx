bd3b8c1b36f32c7d53c5956e1a2980be98797735
Romain <hercez92@hotmail.fr>
2022-04-12 02:46:25 +0200

    add form labels and error message placeholder

--- front/src/pages/Auth/Signup.tsx
+++ front/src/pages/Auth/Signup.tsx
@@ -5,48 +5,46 @@ import { authProvider } from "../../domain/AuthProvider";
 import { SessionContext } from "./context/SessionContext";
 
 export default function Signup() {
-    const { setLoggedIn, createSession } = useContext(SessionContext);
-
-    const [form, setForm] = useState({
+    const signupForm = {
         email: "",
         username: "",
         password: "",
         confirmPassword: "",
-    });
-    const {
-        register,
-        watch,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        reValidateMode: "onBlur",
-        defaultValues: {
-            email: "",
-            username: "",
-            password: "",
-            confirmPassword: "",
-        },
-    });
+    };
+    const { setLoggedIn, createSession } = useContext(SessionContext);
 
+    const [form, setForm] = useState(signupForm);
+    const [formErrors, setFormErrors] = useState(signupForm);
+
+    const resetFormErrors = () => {
+        if (formErrors !== signupForm) {
+            setFormErrors(signupForm);
+        }
+    };
     const navigate = useNavigate();
 
     function changeEmail(event: React.ChangeEvent<HTMLInputElement>) {
+        resetFormErrors();
         const value = event.target.value;
         setForm((form) => ({ ...form, email: value }));
     }
     function changeUsername(event: React.ChangeEvent<HTMLInputElement>) {
+        resetFormErrors();
         const value = event.target.value;
         setForm((form) => ({ ...form, username: value }));
     }
     function changePassword(event: React.ChangeEvent<HTMLInputElement>) {
+        resetFormErrors();
         const value = event.target.value;
         setForm((form) => ({ ...form, password: value }));
     }
     function changeConfirmPassword(event: React.ChangeEvent<HTMLInputElement>) {
+        resetFormErrors();
         const value = event.target.value;
         setForm((form) => ({ ...form, confirmPassword: value }));
     }
     async function postContent(e: React.FormEvent<HTMLFormElement>) {
+        resetFormErrors();
         e.preventDefault();
         if (
             form.email &&
@@ -69,44 +67,73 @@ export default function Signup() {
     }
 
     return (
-        <section className="flex flex-col content-center justify-center border bg-gray-200 border-blue-900 rounded">
-            <form onSubmit={postContent} className="flex flex-col p-4 gap-3">
+        <section className="flex flex-col sm:w-xl sm:mx-auto content-center justify-center border bg-gray-200 border-blue-900 rounded">
+            <form onSubmit={postContent} className="flex flex-col p-5 gap-2">
+                <label htmlFor="email" className="flex flex-col font-bold mt-1">
+                    Email :
+                </label>
                 <input
-                    {...register("email")}
                     type="email"
-                    placeholder="Email"
-                    className="p-2 border border-blue-900 rounded"
+                    name="email"
+                    className="mx-2 p-2 border border-blue-900 rounded"
                     onChange={(event) => changeEmail(event)}
                     value={form.email}
                     required
                 />
+                <p className="mx-4 h-6 font-bold text-red-700">
+                    {formErrors.email}
+                </p>
+                <label
+                    htmlFor="username"
+                    className="flex flex-col font-bold mt-1"
+                >
+                    Nom d'utilisateur :
+                </label>
                 <input
-                    {...register("username")}
-                    placeholder="Nom d'utilisateur"
-                    className="p-2 border border-blue-900 rounded"
+                    name="username"
+                    className="mx-2 p-2 border border-blue-900 rounded"
                     onChange={(event) => changeUsername(event)}
                     value={form.username}
                     required
                 />
+                <p className="mx-4 h-6 font-bold text-red-700">
+                    {formErrors.username}
+                </p>
+                <label
+                    htmlFor="password"
+                    className="flex flex-col font-bold mt-1"
+                >
+                    Mot de passe :
+                </label>
                 <input
-                    {...register("password")}
                     type="password"
-                    placeholder="Mot de passe"
-                    className="p-2 border border-blue-900 rounded"
+                    name="password"
+                    className="mx-2 p-2 border border-blue-900 rounded"
                     onChange={(event) => changePassword(event)}
                     value={form.password}
                     required
                 />
+                <p className="mx-4 h-6 font-bold text-red-700">
+                    {formErrors.password}
+                </p>
+                <label
+                    htmlFor="confirm-password"
+                    className="flex flex-col font-bold mt-1"
+                >
+                    Confirmez le mot de passe :
+                </label>
                 <input
-                    {...register("confirmPassword")}
                     type="password"
-                    placeholder="Confirmez le mot de passe"
-                    className="p-2 border border-blue-900 rounded"
+                    name="confirm-password"
+                    className="mx-2 p-2 border border-blue-900 rounded"
                     onChange={(event) => changeConfirmPassword(event)}
                     value={form.confirmPassword}
                     required
                 />
-                <button className="text-white bg-blue-900 p-2 rounded hover:bg-blue-700 hover:shadow-sm hover:shadow-blue-800">
+                <p className="mx-4 h-6 font-bold text-red-700">
+                    {formErrors.confirmPassword}
+                </p>
+                <button className="text-white bg-blue-900 mx-2 mt-2 p-2 rounded hover:bg-blue-700 hover:shadow-sm hover:shadow-blue-800">
                     Créer un compte
                 </button>
                 <Link
