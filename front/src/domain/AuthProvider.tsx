47a72891656ed4526dba2d611e9e2a587f5aaee3
Romain <hercez92@hotmail.fr>
2022-04-12 02:44:42 +0200

    throw login errors to login component

--- front/src/domain/AuthProvider.tsx
+++ front/src/domain/AuthProvider.tsx
@@ -2,37 +2,41 @@ import axios from "axios";
 
 class AuthProvider {
     constructor() {
-        const localhost = "http://localhost:8000"
-        const localIp = "http://192.168.0.10:8000"
-        const heroku = "https://groupomania-socials.herokuapp.com"
+        const localhost = "http://localhost:8000";
+        const localIp = "http://192.168.0.10:8000";
+        const heroku = "https://groupomania-socials.herokuapp.com";
 
-        axios.defaults.baseURL = `${heroku}`
+        axios.defaults.baseURL = `${heroku}`;
     }
 
     async login(loginInfo): Promise<any> {
-        const loginResponse = await axios.post("/auth/login", loginInfo)
-        return loginResponse
+        try {
+            const loginResponse = await axios.post("/auth/login", loginInfo);
+            return loginResponse;
+        } catch (error) {
+            throw error.response.data;
+        }
     }
 
     async signup(userInfo) {
-        await axios.post("/auth/signup", userInfo)
+        await axios.post("/auth/signup", userInfo);
     }
 
     async relog() {
         try {
-            const relogResponse = await axios.post("auth")
+            const relogResponse = await axios.post("auth");
             if (relogResponse.data) {
-                return relogResponse.data
+                return relogResponse.data;
             }
-            return relogResponse
+            return relogResponse;
         } catch (error) {
-            return error.response.status
+            return error.response.status;
         }
     }
 
     async logout() {
-        await axios.post("auth/logout")
+        await axios.post("auth/logout");
     }
 }
 
-export const authProvider = new AuthProvider()
+export const authProvider = new AuthProvider();
