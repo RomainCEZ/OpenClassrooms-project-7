8bfd901ed23d5d4fa885783c8d2b18142a6983c7
Romain <hercez92@hotmail.fr>
2022-04-13 03:11:35 +0200

    move validation to createUserDto

--- back/src/users/dto/create-user.dto.ts
+++ back/src/users/dto/create-user.dto.ts
@@ -1,11 +1,19 @@
-import { IsNotEmpty, IsString, Length, Matches } from 'class-validator'
-import { LoginUserDto } from './login-user.dto'
+import { IsEmail, IsNotEmpty, Length, Matches, MaxLength } from 'class-validator'
 
-export class CreateUserDto extends LoginUserDto {
+export class CreateUserDto {
+
+    @IsNotEmpty({ message: "Vous devez saisir une adresse email !" })
+    @IsEmail({}, { message: "Vous devez saisir une adresse email valide !" })
+    @MaxLength(100, { message: "Votre adresse email ne doit pas dépasser 100 charactères !" })
+    email: string
 
     @IsNotEmpty({ message: "Vous devez choisir un nom d'utilisateur !" })
-    @IsString()
     @Length(3, 50, { message: "Votre nom d'utilisateur doit contenir entre 3 et 50 charactères !" })
     @Matches(new RegExp(/^[0-9-'\s\p{L}\p{M}]+$/mui), { message: "Votre nom d'utilisateur ne peut contenir que des charactères alphanumériques !" })
     username: string
+
+    @IsNotEmpty({ message: "Vous devez choisir un mot de passe !" })
+    @Length(8, 100, { message: "Votre mot de passe doit contenir entre 8 et 100 charactères !" })
+    @Matches(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), { message: "Votre mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre !" })
+    password: string
 }
--- back/src/users/dto/login-user.dto.ts
+++ back/src/users/dto/login-user.dto.ts
@@ -1,15 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, Length, Matches, MaxLength } from 'class-validator'
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
 
-export class LoginUserDto {
-
-    @IsNotEmpty()
-    @IsEmail()
-    @MaxLength(100, { message: "Votre adresse email ne doit pas dépasser 100 charactères !" })
-    email: string
-
-    @IsNotEmpty()
-    @IsString()
-    @Length(8, 100, { message: "Votre mot de passe doit contenir entre 8 et 100 charactères !" })
-    @Matches(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), { message: "Votre mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre !" })
-    password: string
-}
+export class LoginUserDto extends PartialType(CreateUserDto) { }
