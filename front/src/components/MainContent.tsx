ea8e94a34a62e3788ef940e889f3fa3805c70e02
Romain <hercez92@hotmail.fr>
2022-04-13 03:16:48 +0200

    move login and signup to their own folders

--- front/src/components/MainContent.tsx
+++ front/src/components/MainContent.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "../pages/Auth/Login";
-import Signup from "../pages/Auth/Signup";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
 import Error from "../pages/Error/Error";
 import Home from "../pages/Home/Home";
 import NewPost from "../pages/NewPost/NewPost";
@@ -17,7 +17,6 @@ export default function MainContent() {
                 <Route path="/post/:id" element={<PostView />} />
                 <Route path="newpost" element={<NewPost />} />
                 <Route path="/post/:id/edit" element={<EditPost />} />
-                {/* <Route path="/:id/delete" element={<DeletePost />} /> */}
                 <Route path="/404" element={<Error />} />
                 <Route path="*" element={<Error />} />
             </Routes>
