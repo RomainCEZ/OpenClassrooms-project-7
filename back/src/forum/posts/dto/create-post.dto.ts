74a88609b4e8747ba3e98fa6c84bfc25d7d81d0c
Romain <hercez92@hotmail.fr>
2022-04-13 03:11:20 +0200

    remove validation

--- back/src/forum/posts/dto/create-post.dto.ts
+++ back/src/forum/posts/dto/create-post.dto.ts
@@ -1,12 +1,6 @@
-import { IsNotEmpty, MaxLength } from "class-validator"
-
 export class CreatePostDto {
     id: string
-
-    // @IsNotEmpty({ message: "Vous devez saisir un titre !" })
-    // @MaxLength(255, { message: "Le titre ne peut pas contenir plus de 255 charactères !" })
     title: string
-
     content: string
     imageName?: string
     author: string
