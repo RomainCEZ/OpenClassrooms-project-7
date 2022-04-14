21d9aa1d60b6a4c63c4e1873bb911acaedab28e9
Romain <hercez92@hotmail.fr>
2022-04-15 01:55:49 +0200

    add button type and target

--- front/src/components/Buttons/BlueFormButton.tsx
+++ front/src/components/Buttons/BlueFormButton.tsx
@@ -1,9 +1,10 @@
-export default function BlueFormButton({ children }) {
+export default function BlueFormButton({ children, target }) {
     return (
         <button
-            className="p-2 rounded flex-grow bg-blue-700 text-white font-bold px-6
-            hover:bg-blue-600 focus:bg-blue-600 focus:shadow active:bg-blue-500 active:shadow
-            transition-all"
+            type="submit"
+            form={target}
+            className="p-2 rounded flex-grow bg-blue-700 text-white shadow-md font-bold px-6
+            hover:bg-blue-600 focus:bg-blue-600 active:bg-blue-500 transition-all"
         >
             {children}
         </button>
