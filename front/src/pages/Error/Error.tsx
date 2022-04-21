69b5853c34154bda3235d1943a5d5cada2c17422
Romain <hercez92@hotmail.fr>
2022-04-21 17:45:23 +0200

    change styles

--- front/src/pages/Error/Error.tsx
+++ front/src/pages/Error/Error.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 
 export default function Error() {
-  return (
-    <div className="flex flex-col justify-center items-center p-10 h">
-      <Link to="/" className="text-2xl mb-8 underline text-indigo-900">Retour à l'accueil.</Link>
-      <span className="mb-5 text-9xl font-black text-indigo-900">404</span>
-      <p className="text-4xl text-indigo-900">Cette page n'existe pas !</p>
-    </div>
-  )
+    return (
+        <div className="flex flex-col justify-center items-center p-10 h">
+            <Link
+                to="/"
+                className="font-bold text-3xl mb-8 underline underline-offset-2 decoration-2
+                text-blue-800 hover:text-blue-600 focus:text-blue-600 active:text-blue-500"
+            >
+                Retour à l'accueil
+            </Link>
+            <span className="mb-5 text-9xl font-black text-blue-800">404</span>
+            <p className="text-4xl font-bold text-blue-800">
+                Cette page n'existe pas !
+            </p>
+        </div>
+    );
 }
