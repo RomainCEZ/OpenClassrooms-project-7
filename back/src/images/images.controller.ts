a992a335b922a6c7ab456e1801a1fd3bd0392090
Romain <hercez92@hotmail.fr>
2022-04-24 03:58:02 +0200

    rename images folder to profilepicture

--- back/src/images/images.controller.ts
+++ back/src/images/images.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Param, Res } from "@nestjs/common";
 import { join } from "path";
 
-@Controller("images")
+@Controller("profilepicture")
 export class ImagesController {
   @Get(":imageName")
   findImage(@Param("imageName") imageName: string, @Res() res: any) {
-    return res.sendFile(join(process.cwd(), "images/" + imageName))
+    return res.sendFile(join(process.cwd(), "profilepicture/" + imageName))
   }
 }
