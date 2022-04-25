c38e4c33feb4be95e845f91c18353c193c7359a7
Romain <hercez92@hotmail.fr>
2022-04-25 21:12:40 +0200

    center content

--- front/src/components/DarkmodeWrapper.tsx
+++ front/src/components/DarkmodeWrapper.tsx
@@ -6,7 +6,7 @@ export default function DarkmodeWrapper({ children }) {
 
     return (
         <div className={darkmode ? "dark" : ""}>
-            <div className="px-6 sm:px-20 rounded border shadow-xl bg-white/90 dark:bg-gray-400 border-indigo-900 dark:border-gray-800">
+            <div className="flex flex-col items-center px-6 sm:px-20 rounded border shadow-xl bg-white/90 dark:bg-gray-400 border-indigo-900 dark:border-gray-800">
                 {children}
             </div>
         </div>
