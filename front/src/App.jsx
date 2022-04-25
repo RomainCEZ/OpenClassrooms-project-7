dde63c4ebc15c57e3d82510c869d7d5b29a86225
Romain <hercez92@hotmail.fr>
2022-04-25 23:51:56 +0200

    fix import file name

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import { SessionProvider } from "./pages/Auth/context/SessionContext";
-import { DarkModeContext } from "./components/DarkModeContext";
+import { DarkModeContext } from "./components/Darkmode/DarkModeContext";
 import MainContainer from "./components/MainContainer";
 import MainContent from "./components/MainContent";
 
