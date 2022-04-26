127a831156f3ed2a135cf75823cb63e73e763f3a
Romain <hercez92@hotmail.fr>
2022-04-26 02:09:21 +0200

    lighten menu bg color

--- front/src/components/NavMenu/DropDownNav.tsx
+++ front/src/components/NavMenu/DropDownNav.tsx
@@ -21,7 +21,7 @@ export function DropDownNav() {
                 leaveFrom="transform scale-100 opacity-100"
                 leaveTo="transform scale-95 opacity-0"
             >
-                <Menu.Items className="overflow-hidden absolute mt-8 right-0 w-56 z-20 text-white bg-blue-800 dark:bg-gray-800 border-blue-500 dark:border-gray-600 divide-blue-500 dark:divide-gray-600 divide-y  rounded-lg shadow-lg">
+                <Menu.Items className="overflow-hidden absolute mt-8 right-0 w-56 z-20 text-white bg-blue-700 dark:bg-gray-800 border-blue-500 dark:border-gray-600 divide-blue-500 dark:divide-gray-600 divide-y  rounded-lg shadow-lg">
                     <NavLink path={`./`}>Accueil</NavLink>
                     <NavLink path="./profile">Mon profil</NavLink>
                     <DarkModeToggle />
