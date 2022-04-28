f239956094b6effeb9181b59595d7bde34264878
Romain <hercez92@hotmail.fr>
2022-04-28 04:59:36 +0200

    add icons to buttons and link to logo

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,26 +3,44 @@ import { Link } from "react-router-dom";
 import { Logo } from "../assets/Logo";
 import { SessionContext } from "../pages/Auth/context/SessionContext";
 import { DropDownNav } from "./NavMenu/DropDownNav";
+import { FaHome } from "react-icons/fa";
+import { BiLogIn } from "react-icons/bi";
 
 export default function Header() {
     const { loggedIn } = useContext(SessionContext);
 
     return (
-        <header className="flex items-center justify-between h-32 bg-blue-800 dark:bg-gray-800 px-8">
-            <Logo />
-            <nav className="relative">
+        <header className="flex items-center justify-between h-32 bg-blue-800 dark:bg-gray-800 pl-9 pr-6 sm:px-8">
+            <Link to="/" className="z-30">
+                <Logo />
+            </Link>
+            <nav className="relative z-20">
                 {loggedIn ? (
                     <DropDownNav />
                 ) : (
                     <ul className="flex font-bold divide-x-2 divide-gray-100 dark:divide-gray-600">
                         <li>
-                            <Link to="/" className="mr-4 px-8 btn blue">
-                                Accueil
+                            <Link
+                                to="/"
+                                className="flex items-center mr-4 px-3 lg:px-7 btn blue gap-2"
+                            >
+                                <span className="text-3xl md:text-2xl lg:hidden">
+                                    <FaHome />
+                                </span>{" "}
+                                <span className="hidden lg:block">Accueil</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="./login" className="ml-4 px-6 btn blue">
-                                Connexion
+                            <Link
+                                to="./login"
+                                className="flex items-center ml-4 pl-2 pr-4 md:px-6 btn blue gap-2"
+                            >
+                                <span className="text-3xl md:text-2xl md:hidden">
+                                    <BiLogIn />
+                                </span>{" "}
+                                <span className="hidden md:block">
+                                    Connexion
+                                </span>
                             </Link>
                         </li>
                     </ul>
