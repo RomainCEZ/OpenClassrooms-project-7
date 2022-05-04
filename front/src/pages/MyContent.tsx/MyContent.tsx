c6d10fe660e8a7ce960c49ea5a49cb0b9b4fc45d
Romain <hercez92@hotmail.fr>
2022-05-04 02:01:05 +0200

    move MyContent logic to useMyContentHook

--- front/src/pages/MyContent.tsx/MyContent.tsx
+++ front/src/pages/MyContent.tsx/MyContent.tsx
@@ -1,66 +1,52 @@
 import { Tab } from "@headlessui/react";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { apiProvider } from "../../providers/ApiProvider";
 import MyComments from "./MyComments";
+import MyFavorites from "./MyFavorites";
 import MyPosts from "./MyPosts";
+import { useMyContentHook } from "./useMyContentHook";
 
 const MyContent = () => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const navigate = useNavigate();
-    const [posts, setPosts] = useState([]);
-    const [comments, setComments] = useState([]);
-
-    const handleChange = (index) => {
-        setSelectedIndex(index);
-        index === 0 ? openPosts() : openComments();
-    };
-
-    const openPosts = () => {
-        navigate("/mycontent/posts");
-        if (posts.length === 0) {
-            apiProvider.getMyPosts().then((posts) => setPosts(posts));
-        }
-    };
-
-    const openComments = () => {
-        navigate("/mycontent/comments");
-        if (comments.length === 0) {
-            apiProvider
-                .getMyComments()
-                .then((comments) => setComments(comments));
-        }
-    };
-
-    useEffect(() => {
-        const page = document.location.pathname.split("/mycontent/")[1];
-        if (page === "posts") {
-            setSelectedIndex(0);
-            apiProvider.getMyPosts().then((posts) => setPosts(posts));
-            return;
-        }
-        if (page === "comments") {
-            setSelectedIndex(1);
-            apiProvider
-                .getMyComments()
-                .then((comments) => setComments(comments));
-            return;
-        }
-        navigate("/404");
-    }, []);
+    const [selectedIndex, setSelectedIndex, data] = useMyContentHook(
+        [
+            {
+                id: "0",
+                slug: "posts",
+                api: apiProvider.getMyPosts,
+            },
+            {
+                id: "1",
+                slug: "comments",
+                api: apiProvider.getMyComments,
+            },
+            {
+                id: "2",
+                slug: "favorites",
+                api: apiProvider.getFavorites,
+            },
+        ],
+        { posts: [], comments: [], favorites: [] }
+    );
 
     return (
         <Tab.Group
-            selectedIndex={selectedIndex}
-            onChange={(index) => handleChange(index)}
+            selectedIndex={+selectedIndex}
+            onChange={(index) => setSelectedIndex(index.toString())}
         >
-            <Tab.List className="flex gap-2 mb-2">
-                <Tab className="btn white w-1/2">Publications</Tab>
-                <Tab className="btn white w-1/2">Commentaires</Tab>
+            <Tab.List className="flex flex-wrap gap-2 mb-3">
+                <Tab className="flex-grow btn white min-w-fit px-4">
+                    Publications
+                </Tab>
+                <Tab className="flex-grow btn white min-w-fit px-4">
+                    Commentaires
+                </Tab>
+                <Tab className="flex-grow btn white min-w-fit px-4">
+                    Favoris
+                </Tab>
             </Tab.List>
             <Tab.Panels>
-                <Tab.Panel>{<MyPosts posts={posts} />}</Tab.Panel>
-                <Tab.Panel>{<MyComments comments={comments} />}</Tab.Panel>
+                <Tab.Panel>{<MyPosts posts={data.posts} />}</Tab.Panel>
+                <Tab.Panel>{<MyComments comments={data.comments} />}</Tab.Panel>
+                <Tab.Panel>{<MyFavorites posts={data.favorites} />}</Tab.Panel>
             </Tab.Panels>
         </Tab.Group>
     );
