b0d4818bf63cc01399488ae98a5a6c9930e8c3da
Romain <hercez92@hotmail.fr>
2022-05-09 21:45:07 +0200

    add value

--- front/src/pages/Post/FavoriteStar.tsx
+++ front/src/pages/Post/FavoriteStar.tsx
@@ -2,7 +2,11 @@ import { FaStar, FaRegStar } from "react-icons/fa";
 
 export default function FavoriteStar({ onClick, isActive, className }) {
     return (
-        <button onClick={onClick} className={className}>
+        <button
+            onClick={onClick}
+            className={className}
+            value={isActive ? "Retirer des favoris" : "Ajouter aux favoris"}
+        >
             {isActive ? (
                 <span className="text-yellow-400 hover:text-yellow-500 transition">
                     <FaStar />
